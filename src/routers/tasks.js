dd14f40bc3201e9c1c97ea434df049d83f5f7425
Pablo Fuentes <pfuentes@eagles.bridgewater.edu>
2022-03-25 11:48:16 -0400

    Added task modification and deletion -Needs testing-

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
+const { request } = require('express')
+const MongoClient = require('mongodb').MongoClient
+
 
 const router = new express.Router()
 
@@ -33,7 +36,7 @@ router.get('/tasks', auth, async(req, res) => {
         match.completed = (req.query.completed === 'true')
     }
     try {
-        console.log('test')
+        //console.log('test')
         await req.user.populate({
             path: 'tasks',
             match,
@@ -50,5 +53,96 @@ router.get('/tasks', auth, async(req, res) => {
         res.status(500).send()
     }
 })
+
+
+
+router.patch('/tasks', auth, async(req, res) => {
+    const user = req.user
+    const task = await Task.findOne({_id:req.body._id})
+    const mods = req.body
+    const props = Object.keys(mods)
+
+    /*MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
+        const dbo = db.db("web-app-api")
+        var temptask = dbo.collection('tasks').findOne({'title': req.body.title}).then(request => {
+            console.log(request)
+            console.log(temptask)       #### Leftover from overly complicated attempt, could not find by _id ####
+        })
+        console.log(props) */
+    
+    
+    const modifiable = ['_id', 'title', 'description', 'completed']
+    const isValid = props.every((prop) => modifiable.includes(prop))
+    const validUser = task.owner.equals(user._id)
+    
+    if (!isValid || !validUser) {
+        return res.status(400).send({ error: 'Invalid updates.' })
+    }
+  
+    try {
+            /*dbo.collection('tasks').findOneAndUpdate(     #### Leftover from overly complicated attempt ####
+                {"title":req.body.title},
+                {$set:{"description": req.body.description, "completed": req.body.completed}},
+            )*/ 
+
+        props.forEach((prop) => task[prop] = mods[prop])
+        await task.save()
+        res.send(task)
+    } catch (e) {
+        console.log(e)
+        res.status(400).send()
+    }
+    res.status(200).send()
+//})
+
+})
+    
+
+
+
+    
+router.delete('/tasks', auth, async(req, res) => {
+    const user = req.user
+    const task = await Task.findOne({_id:req.body._id})
+
+    /*MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
+        
+        const dbo = db.db("web-app-api")
+        var look = req.body._id;
+        var temptask = dbo.collection('tasks').findOne({'title': req.body.title}).then(request => {
+            
+            console.log(request)            #### Same as in the patch endpoint ####
+            //console.log(temptask)
+        }) */
+    
+    
+    const isValid = task.owner.equals(user._id)
+    if (!isValid) {
+        return res.status(400).send({ error: 'Invalid updates.' })
+    }
+  
+    try {
+            /*dbo.collection('tasks').findOneAndDelete(
+                {"title":"B"},
+                {
+                    writeConcern: {
+                        w: 1,
+                        j:true,
+                        wtimeout: 1000
+                    }
+                }
+            )*/
+
+        await Task.deleteOne({_id: req.body._id})
+        res.send(req.task)
+        console.log("----------------------------------")
+    } catch (e) {
+        console.log(e)
+        res.status(400).send()
+    }
+    res.status(200).send()
+    //})
+})
+
 
 module.exports = router
